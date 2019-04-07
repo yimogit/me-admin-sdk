930d758ca65c0f348ecb3da5cf0828d561daab4d
yimo <wsyimo@qq.com>
2019-04-07 21:14:23 +0800

    fix select comp catch

--- src/components/VForm/Select.vue
+++ src/components/VForm/Select.vue
@@ -45,20 +45,20 @@ export default {
   watch: {
     value: {
       handler(val) {
-        var isLoad = val === 0
+        var isLoad = val === 0;
         if (this.multiple) {
           if (this.multipleSplit) {
-            this.currentValue = (val || '')
+            this.currentValue = (val || "")
               .split(this.multipleSplit)
-              .filter(s => s.trim() !== '')
+              .filter(s => s.trim() !== "");
           } else {
-            this.currentValue = (val || []).map(e => String(e))
+            this.currentValue = (val || []).map(e => String(e));
           }
         } else {
-          this.currentValue = String(isLoad ? '0' : val || '')
+          this.currentValue = String(isLoad ? "0" : val || "");
         }
         if (isLoad || (this.currentValue && this.currentValue.length > 0)) {
-          this.loadData()
+          this.loadData();
         }
       },
       immediate: true
@@ -69,28 +69,32 @@ export default {
       options: [],
       currentValue: this.multiple ? [] : null,
       isloading: false
-    }
+    };
   },
   methods: {
     change(val) {
-      if (typeof this.value === 'string' && this.multipleSplit) {
-        this.$emit('input', val.join(','))
+      if (typeof this.value === "string" && this.multipleSplit) {
+        this.$emit("input", val.join(","));
       } else {
-        this.$emit('input', val)
+        this.$emit("input", val);
       }
     },
     loadData(autoOpen) {
-      if (this.isloading || this.options.length > 0 || !this.apiFunc) return
-      this.isloading = true
-      this.apiFunc().then(res => {
-        this.options = res.data.map(s => {
-          s.value = String(s.value)
-          return s
+      if (this.isloading || this.options.length > 0 || !this.apiFunc) return;
+      this.isloading = true;
+      this.apiFunc()
+        .then(res => {
+          this.options = res.data.map(s => {
+            s.value = String(s.value);
+            return s;
+          });
+          this.isloading = false;
+          if (autoOpen) this.$refs.currentSelect.selectOption();
         })
-        this.isloading = false
-        if (autoOpen) this.$refs.currentSelect.selectOption()
-      })
+        .catch(_ => {
+          this.isloading = false;
+        });
     }
   }
-}
+};
 </script>
